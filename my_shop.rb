b43d1357beb0edcafab5b0321155d7e957d8c7b5
Jan Molak <jan.molak@gmail.com>
2011-10-03 21:32:59 +0100

    added my_shop.rb

--- my_shop.rb
+++ my_shop.rb
@@ -1,3 +1,30 @@
-class MyShop
-  # To change this template use File | Settings | File Templates.
+require 'sinatra/base'
+require 'dm-validations'
+require 'dm-timestamps'
+require 'dm-migrations'
+require 'haml'
+require 'less'
+
+#
+# DataMapper
+#
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/shop.db")
+
+module RubyTuesday
+  include DataMapper
+
+  class MyShopApp < Sinatra::Base
+    set :haml, :format => :html5
+    set :static => true
+    set :public, File.dirname(__FILE__) + '/public'
+
+    get '/' do
+
+    end
+
+    get %r{/([\w]+)\.css} do
+      less :"less/#{params[:captures].first}"
+    end
+  end
+
 end
